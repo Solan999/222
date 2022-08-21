85740507aaf952f9bd0f730c884df94ecfcd629d
Solan999 <103107845+Solan999@users.noreply.github.com>
2022-08-22 02:23:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,4 @@ wea, temperature = get_weather()
 data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
+print("words")
